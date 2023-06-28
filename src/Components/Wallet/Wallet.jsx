2a2b841d9e7fcc522ddb7e57117d2e85bd6d66c7
Sarthak <sarthakshukla72@gmail.com>
2023-06-28 09:39:17 +0530

    work on ui and functionalities

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -6,6 +6,7 @@ import Navbar from "../Navbar/Navbar";
 import { FlipDate } from "../flipTimer/FlipDate";
 import { FlipMoney } from "../flipTimer/FlipMoney";
 import balance from "../../assets/imgs/balance.svg";
+import addAmt from "../../assets/imgs/addAmt.svg"
 
 const Wallet = () => {
     return (
@@ -18,7 +19,71 @@ const Wallet = () => {
                         My Wallet
                     </p>
 
-                    
+                    <div className="flex justify-center items-center mt-5 flex-col">
+                        <img src="/img/balance.svg" width={205} />
+                        
+                        <div className="mt-6">
+                            <img src={balance} alt="" />
+                        </div>
+
+                        <div className="add_money profile_submit mt-6">
+                            <button type="submit">Add Money</button>
+                        </div>
+                    </div>
+
+                    <div className="transactions">
+                        <div className="head flex justify-between mt-12">
+                            <p className="text-white text-xl">Transaction History</p>
+                            <p className="font-light text-sm text-red-600 underline">View All</p>
+                        </div>
+
+                        <div className="transaction_info mt-6">
+                            <div className="info">
+                                <div className="info_left">
+                                    <div className="left_img"><img src={addAmt} alt="" /></div>
+                                    <span>Add Amount</span>
+                                </div>
+
+                                <div className="info_right">
+                                    <div className="amount">₹4000</div>
+                                    <div className="date font-light mt-3" style={{fontSize: '9px'}}>23-July-2023 10:00 AM</div>
+                                </div>
+                            </div>
+                            <div className="info">
+                                <div className="info_left">
+                                    <div className="left_img"><img src={addAmt} alt="" /></div>
+                                    <span>Add Amount</span>
+                                </div>
+
+                                <div className="info_right">
+                                    <div className="amount">₹4000</div>
+                                    <div className="date font-light mt-3" style={{fontSize: '9px'}}>23-July-2023 10:00 AM</div>
+                                </div>
+                            </div>
+                            <div className="info">
+                                <div className="info_left">
+                                    <div className="left_img"><img src={addAmt} alt="" /></div>
+                                    <span>Add Amount</span>
+                                </div>
+
+                                <div className="info_right">
+                                    <div className="amount">₹4000</div>
+                                    <div className="date font-light mt-3" style={{fontSize: '9px'}}>23-July-2023 10:00 AM</div>
+                                </div>
+                            </div>
+                            <div className="info">
+                                <div className="info_left">
+                                    <div className="left_img"><img src={addAmt} alt="" /></div>
+                                    <span>Add Amount</span>
+                                </div>
+
+                                <div className="info_right">
+                                    <div className="amount">₹4000</div>
+                                    <div className="date font-light mt-3" style={{fontSize: '9px'}}>23-July-2023 10:00 AM</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
